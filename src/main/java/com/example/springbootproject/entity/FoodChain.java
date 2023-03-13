34d0915aafbc271c7d126c3e0dfe3b2edfc9d72f
William Karlström <wkarlstrom@outlook.com>
2023-03-13 14:50:27 +0100

    Add FoodChain entity

---
+++ src/main/java/com/example/springbootproject/entity/FoodChain.java
@@ -0,0 +1,35 @@
+package com.example.springbootproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class FoodChain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
