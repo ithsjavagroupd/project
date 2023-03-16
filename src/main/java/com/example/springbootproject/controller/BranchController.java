5b03bf73976313875a2ceeb00e27828448c16a41
RehasLiamsi <nsaher91@gmail.com>
2023-03-16 13:54:06 +0100

    Squashed commits

--- src/main/java/com/example/springbootproject/controller/BranchController.java
+++ src/main/java/com/example/springbootproject/controller/BranchController.java
@@ -1,4 +1,60 @@
 package com.example.springbootproject.controller;
 
+
+import com.example.springbootproject.entity.Branch;
+import com.example.springbootproject.repository.BranchRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/branch")
 public class BranchController {
+    private final BranchRepository repository;
+
+    public BranchController(BranchRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/{id}")
+    Branch getABranch(@PathVariable long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @GetMapping
+    List<Branch> getBranches() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    void addBranch(@RequestBody Branch branch) {
+        String name = branch.getName();
+        if (name == null || name.isEmpty())
+            throw new IllegalStateException();
+        repository.save(branch);
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteBranch(@PathVariable Long id) {
+        if (repository.findById(id).isPresent())
+            repository.deleteById(id);
+        else
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Branch> updateBranch(@PathVariable Long id, @RequestBody Branch branchDetails) {
+        var updateBranch = repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        updateBranch.setName(branchDetails.getName());
+
+        repository.save(updateBranch);
+        return ResponseEntity.ok(updateBranch);
+    }
 }
+
+
---
+++ src/main/java/com/example/springbootproject/entity/Branch.java
@@ -0,0 +1,20 @@
+package com.example.springbootproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String name;
+}
