98b3999189c95bc7451bb77c6f67c02d4c56d7b1
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-03-17 08:15:21 +0100

    Comment out ApplicationTests

--- src/test/java/com/example/springbootproject/SpringBootProjectApplicationTests.java
+++ src/test/java/com/example/springbootproject/SpringBootProjectApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.springbootproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringBootProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class SpringBootProjectApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
