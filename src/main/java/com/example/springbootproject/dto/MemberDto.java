bd45a7713b0d620982e28069474fd2c72588b600
RehasLiamsi <nsaher91@gmail.com>
2023-03-22 11:21:55 +0100

    Add MemberDto

---
+++ src/main/java/com/example/springbootproject/dto/MemberDto.java
@@ -0,0 +1,18 @@
+package com.example.springbootproject.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDto {
+
+    private long id;
+    @NonNull
+    private String name;
+
+    public MemberDto(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
