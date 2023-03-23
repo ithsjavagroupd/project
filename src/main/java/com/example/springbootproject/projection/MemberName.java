e719834014a544e2f9dfc276beaf89b2ef96ea82
bobster <bob.hogvall@iths.se>
2023-03-23 12:10:29 +0100

    Create interface dto for all but employee and add methods to repository

---
+++ src/main/java/com/example/springbootproject/projection/MemberName.java
@@ -0,0 +1,5 @@
+package com.example.springbootproject.projection;
+import com.example.springbootproject.entity.Member;
+public interface MemberName {
+    String getName();
+}
--- src/main/java/com/example/springbootproject/repository/BranchRepository.java
+++ src/main/java/com/example/springbootproject/repository/BranchRepository.java
@@ -1,8 +1,15 @@
 package com.example.springbootproject.repository;
 
 import com.example.springbootproject.entity.Branch;
+import com.example.springbootproject.projection.BranchName;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface BranchRepository extends ListCrudRepository<Branch, Long> {
+
+    BranchName findNamesBy();
+
+    List<BranchName> findAllNamesBy();
 
 }
