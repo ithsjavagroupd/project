662a85a7e79cea98b4ec3a04cddad756b93b1e79
bobster <bob.hogvall@iths.se>
2023-03-23 15:20:40 +0100

    add projection for members

---
+++ src/main/java/com/example/springbootproject/projection/MemberName.java
@@ -0,0 +1,5 @@
+package com.example.springbootproject.projection;
+
+public interface MemberName {
+    String getName();
+}
---
+++ src/main/java/com/example/springbootproject/projection/StoreName.java
@@ -0,0 +1,5 @@
+package com.example.springbootproject.projection;
+
+public interface StoreName {
+    String getName();
+}
