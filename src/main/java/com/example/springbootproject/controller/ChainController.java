fcab378796a4d0af0f33cd72f9afb73ca0cf32db
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-03-28 14:27:54 +0200

    Add tests for Delete and Put
    Co-authored-by: Svartkettil <98671061+Svartkettil@users.noreply.github.com>

--- src/main/java/com/example/springbootproject/controller/ChainController.java
+++ src/main/java/com/example/springbootproject/controller/ChainController.java
@@ -15,6 +15,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 @RestController
 @RequestMapping("/chains")
 public class ChainController {
@@ -87,11 +89,11 @@ public class ChainController {
 
         updateChain.setName(chain.getName());
         updateChain.setAddress(chain.getAddress());
+        updateChain.setMembers(chain.getMembers());
 
         repository.save(updateChain);
 
         return ResponseEntity.ok(updateChain);
-
     }
 
     @PutMapping("{chainId}/members/{memberId}")
--- src/test/java/com/example/springbootproject/controller/ChainControllerTest.java
+++ src/test/java/com/example/springbootproject/controller/ChainControllerTest.java
@@ -1,38 +1,44 @@
 package com.example.springbootproject.controller;
 
 import com.example.springbootproject.entity.Chain;
+import com.example.springbootproject.entity.Member;
 import com.example.springbootproject.repository.ChainRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = ChainController.class)
 class ChainControllerTest {
     Chain chain1;
     Chain chain2;
+    TestRestTemplate restTemplate;
 
     @Autowired
     MockMvc mockmvc;
@@ -40,8 +46,6 @@ class ChainControllerTest {
     @MockBean
     ChainRepository repository;
 
-    @MockBean
-    ChainController controller;
 
     @BeforeEach
     public void init() {
@@ -73,14 +77,30 @@ class ChainControllerTest {
     @Test
     void addChainShouldReturn201() throws Exception {
 
-        mockmvc.perform(MockMvcRequestBuilders
-                .post("/chains")
+        mockmvc.perform(post("/chains")
                         .contentType(APPLICATION_JSON)
                         .content(asJsonString(chain1))
-                .accept(APPLICATION_JSON))
+                        .accept(APPLICATION_JSON))
                 .andExpect(status().isCreated())
-                .andReturn();
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", "http://localhost/chains/1"))
+                .andExpect(status().isCreated());
     }
+
+    @Test
+    void addChainFunctionTestIntegrationSomething() {
+
+        HttpEntity<Chain> entity = new HttpEntity<>(chain1);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "http://localhost:8080/chains", HttpMethod.POST, entity, String.class);
+
+        String actual = Objects.requireNonNull(response.getHeaders().get(HttpHeaders.LOCATION)).get(0);
+
+        assertTrue(actual.contains("/chains"));
+
+    }
+
 
     public static String asJsonString(final Object obj) {
         try {
@@ -90,4 +110,34 @@ class ChainControllerTest {
         }
     }
 
+
+    @Test
+    void addChainWithInvalidBodyShouldReturn400BadRequest() throws Exception {
+        mockmvc.perform(post("/chains")
+                        .contentType(APPLICATION_JSON)
+                        .content(asJsonString(new String[]{"test"}))
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+    @Test
+    void deleteChainShouldResultInTheNonExistensOfDeletedChainAlsoReturn200Ok() throws Exception{
+        willDoNothing().given(repository).deleteById(chain1.getId());
+        ResultActions response = mockmvc.perform(delete("/chains/{id}", chain1.getId()));
+        response.andExpect(status().isOk());
+    }
+
+    @Test
+    void updateChainShouldReturn200Ok() throws Exception {
+        when(repository.findById(1L)).thenReturn(Optional.of(chain1));
+        Chain updateChain = new Chain();
+        updateChain.setId(1L);
+        updateChain.setName("First Kedjan New Name");
+        updateChain.setAddress("Address 1.5");
+        mockmvc.perform(put("/chains/{id}" , updateChain.getId())
+                .contentType(APPLICATION_JSON)
+                .content(asJsonString(updateChain))
+                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
 }
